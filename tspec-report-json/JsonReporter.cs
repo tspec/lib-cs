0eba39140de5d0b5a7efb6a1af02fa4705ca406a
Ziya Suzen <ziya@suzen.net>
2022-05-20 12:12:31 +0100

    Added timestamp to report

--- tspec-report-json/JsonReporter.cs
+++ tspec-report-json/JsonReporter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Text.Json;
 using Tspec.Core;
@@ -8,6 +9,10 @@ namespace Tspec.Report.Json
     public class JsonReporter
     {
         private readonly Utf8JsonWriter _writer;
+        private int _passed;
+        private int _failed;
+        private bool _currentSuccess;
+        private readonly Stopwatch _stopwatch = new Stopwatch();
 
         public JsonReporter(Stream stream)
         {
@@ -19,7 +24,9 @@ namespace Tspec.Report.Json
 
         public void StartReport()
         {
+            _stopwatch.Restart();
             _writer.WriteStartObject(); // { // root
+            _writer.WriteString("timestamp", DateTime.Now.ToString("u"));
             _writer.WriteStartArray("specs"); // [
         }
 
@@ -28,10 +35,13 @@ namespace Tspec.Report.Json
             _writer.WriteStartObject(); // { // spec
             _writer.WriteString("name", name);
             _writer.WriteStartArray("steps"); // [
+            _currentSuccess = true;
         }
         
         public void AddResult(Result result)
         {
+            if (_currentSuccess) 
+                _currentSuccess = result.Success;
             _writer.WriteStartObject();
             _writer.WriteString("text", result.Text);
             _writer.WriteBoolean("success", result.Success);
@@ -41,13 +51,21 @@ namespace Tspec.Report.Json
 
         public void EndSpec()
         {
+            if (_currentSuccess)
+                _passed++;
+            else
+                _failed++;
             _writer.WriteEndArray(); // ] // steps
             _writer.WriteEndObject(); // } // spec
         }
 
         public void EndReport()
         {
+            _stopwatch.Stop();
             _writer.WriteEndArray(); // ] // specs
+            _writer.WriteString("duration", _stopwatch.Elapsed.ToString());
+            _writer.WriteNumber("passed", _passed);
+            _writer.WriteNumber("failed", _failed);
             _writer.WriteEndObject(); // } // root
             _writer.Flush();
         }
